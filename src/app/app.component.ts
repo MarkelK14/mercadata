0fed62b7c10468439b57660d9b958cbd37a4dcf6
Markel Cortajarena <markelkortajarena@gmail.com>
2025-09-03 19:14:42 +0200

    feat: add FilterComponent and integrate Flowbite for enhanced UI elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FilterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'mercadata';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
